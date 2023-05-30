9da4a549ee7dca582c869c29e4f0f1c3ed5c651d
jitendragangurde <132916016+jitendragangurde@users.noreply.github.com>
2023-05-30 09:45:55 +0530

    Add files via upload

---
+++ scipt.js
@@ -0,0 +1,17 @@
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    loginForm.classList.remove('active');
+}
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+}
+windo.onscroll = () =>{
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+}
